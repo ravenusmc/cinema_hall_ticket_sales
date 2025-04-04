840248683598d733b2f4f2cc921f2d8e72727aad
ravenusmc <mcuddy77@gmail.com>
2025-04-04 15:31:21 -0400

    started work on first graph

---
+++ client/src/components/graphs/TicketSales.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        Bar Chart: Ticket Sales by Movie Genre – Show how many tickets were sold per genre.
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+</style>
