d9a8b55a3f1428d770930e3ac7f02007deedfc61
ravenusmc <mcuddy77@gmail.com>
2025-04-05 10:59:49 -0400

    started work on ticket sales by genre component

--- client/src/components/graphs/TicketSales.vue
+++ client/src/components/graphs/TicketSales.vue
@@ -1,11 +1,14 @@
 <template>
     <div>
+        <h1>Ticket Sales by Movie Genre</h1>
         Bar Chart: Ticket Sales by Movie Genre – Show how many tickets were sold per genre.
     </div>
 </template>
 
 <script>
-
+export default {
+  name: "TicketSales",
+};
 </script>
 
 <style scoped>
--- client/src/views/DataView.vue
+++ client/src/views/DataView.vue
@@ -1,11 +1,24 @@
 <template>
     <div>
-
+      <h1>Data</h1>
+      <TicketSales />
     </div>
 </template>
 
 <script>
+import TicketSales from '@/components/graphs/TicketSales.vue'
 
+export default {
+	name: "DataView",
+  // computed: {
+  //   ...mapGetters("datapage", [
+  //     "hideInjuryGraph",
+  //   ]),
+  // },
+	components: {
+    TicketSales,
+  },
+};
 </script>
 
 <style scoped>
