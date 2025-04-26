3ca5e999cdbddb61db6cdfa7649f54074534de2b
ravenusmc <mcuddy77@gmail.com>
2025-04-26 11:12:58 -0400

    finished pie chart for repeat customers

--- client/src/components/graphs/RepeatCustomer.vue
+++ client/src/components/graphs/RepeatCustomer.vue
@@ -19,124 +19,107 @@ export default {
     this.buildRepeatCustomerGraph();
   },  
   methods: {
-    buildRepeatCustomerGraph() {
-      
-      // set the dimensions and margins of the graph
-      let margin = { top: 50, right: 30, bottom: 50, left: 70 };
-      let width = 460 - margin.left - margin.right;
-      let height = 400 - margin.top - margin.bottom;
+  buildRepeatCustomerGraph() {
+    // set the dimensions and margins of the graph
+    let margin = { top: 50, right: 30, bottom: 50, left: 70 };
+    let width = 460 - margin.left - margin.right;
+    let height = 400 - margin.top - margin.bottom;
+    let radius = Math.min(width, height) / 2; // <-- [CHANGED] Define radius for pie chart
 
-      // append the svg object to the div
-      let svg = d3
-        .select(this.$refs.repeatCustomerGraph)
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-      
-      // Add X axis
-      let x = d3
-        .scaleBand()
-        .range([0, width])
-        .domain(this.repeatCustomersData.map((d) => d[0]))
-        .padding(0.2);
-      svg
-        .append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-      
-      // Add Y axis
-      let y = d3
-        .scaleLinear()
-        .domain([0, d3.max(this.repeatCustomersData, (d) => d[1])])
-        .range([height, 0]);
-      svg.append("g").call(d3.axisLeft(y));
-      
-      // Create a tooltip div
-      let tooltip = d3
-        .select(this.$refs.repeatCustomerGraph)
-        .append("div")
-        .style("opacity", 0)
-        .attr("class", "tooltip")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "1px")
-        .style("border-radius", "5px")
-        .style("padding", "10px")
-        .style("position", "absolute");
-    
-        // Tooltip functions
-      let showTooltip = function (event, d) {
-        tooltip
-          .style("opacity", 1)
-          .html("Genre: " + d[0] + "<br>Ticket Sales: " + d[1])
-          .style("left", event.pageX + 10 + "px")
-          .style("top", event.pageY - 10 + "px");
-      };
+    // append the svg object to the div
+    let svg = d3
+      .select(this.$refs.repeatCustomerGraph)
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr(
+        "transform",
+        "translate(" + (width / 2 + margin.left) + "," + (height / 2 + margin.top) + ")"
+      ); // <-- [CHANGED] Move g to center for pie chart
 
-      let moveTooltip = function (event) {
-        tooltip
-          .style("left", event.pageX + 10 + "px")
-          .style("top", event.pageY - 10 + "px");
-      };
+    // Create a tooltip div
+    let tooltip = d3
+      .select(this.$refs.repeatCustomerGraph)
+      .append("div")
+      .style("opacity", 0)
+      .attr("class", "tooltip")
+      .style("background-color", "white")
+      .style("border", "solid")
+      .style("border-width", "1px")
+      .style("border-radius", "5px")
+      .style("padding", "10px")
+      .style("position", "absolute");
 
-      let hideTooltip = function () {
-        tooltip.style("opacity", 0);
-      };
+    // Tooltip functions
+    let showTooltip = function (event, d) {
+      tooltip
+        .style("opacity", 1)
+        .html("Age Group: " + d.data[0] + "<br>Count: " + d.data[1]) // <-- [CHANGED] Access data differently for pie
+        .style("left", event.pageX + 10 + "px")
+        .style("top", event.pageY - 10 + "px");
+    };
 
-      // Add bars
-      let bars = svg
-        .selectAll("rect")
-        .data(this.repeatCustomersData);
-      
-        // Enter new bars
-        bars
-          .enter()
-          .append("rect")
-          .attr("x", (d) => x(d[0]))
-          .attr("y", height) // Initial position at the bottom of the chart
-          .attr("width", x.bandwidth())
-          .attr("height", 0) // Initial height 0 (so it grows with the animation)
-          .attr("fill", "#0B90CA")
-          .on("mouseover", showTooltip)
-          .on("mousemove", moveTooltip)
-          .on("mouseleave", hideTooltip)
-          .transition() // Apply transition for the animation
-          .duration(1500)
-          .attr("y", (d) => y(d[1])) // Final Y position
-          .attr("height", (d) => height - y(d[1])); // Final height after transition
-            
-      // Add X axis label
-      svg
-        .append("text")
-        .attr("text-anchor", "middle")
-        .attr("x", width / 2)
-        .attr("y", height + margin.bottom - 10) // Adjusted y position to be within the SVG
-        .attr("font-size", "12px")
-        .attr("font-weight", "bold")
-        .text("Age Group");
-      
-      // Add Y axis label
-      svg
-        .append("text")
-        .attr("text-anchor", "middle")
-        .attr("transform", "rotate(-90)")
-        .attr("x", -height / 2)
-        .attr("y", -margin.left + 20)
-        .attr("font-size", "12px")
-        .attr("font-weight", "bold")
-        .text("Count");
-      
-      //title
-      svg
-        .append("text")
-        .attr("text-anchor", "middle")
-        .attr("x", width / 2)
-        .attr("y", -margin.top / 2 + 10) // Adjusted y position to be within the SVG
-        .attr("font-size", "16px")
-        .attr("font-weight", "bold")
-        .text("Repeat Customers");
+    let moveTooltip = function (event) {
+      tooltip
+        .style("left", event.pageX + 10 + "px")
+        .style("top", event.pageY - 10 + "px");
+    };
+
+    let hideTooltip = function () {
+      tooltip.style("opacity", 0);
+    };
+
+    // Generate the pie
+    let pie = d3.pie()
+      .value(function(d) { return d[1]; }); // <-- [CHANGED] Pie values based on counts
+
+    let data_ready = pie(this.repeatCustomersData); // <-- [CHANGED] Prepare the pie chart data
+
+    // Shape generator
+    let arc = d3.arc()
+      .innerRadius(0) // <-- [CHANGED] Set to 0 for regular pie (not donut)
+      .outerRadius(radius);
+
+    // Color scale
+    let color = d3.scaleOrdinal()
+      .domain(this.repeatCustomersData.map(d => d[0]))
+      .range(d3.schemeCategory10); // <-- [NEW] Color for each slice
+
+    // Build the pie chart
+    svg
+      .selectAll('slices')
+      .data(data_ready)
+      .enter()
+      .append('path')
+      .attr('d', arc)
+      .attr('fill', d => color(d.data[0]))
+      .attr("stroke", "white")
+      .style("stroke-width", "2px")
+      .on("mouseover", showTooltip)
+      .on("mousemove", moveTooltip)
+      .on("mouseleave", hideTooltip);
+
+    // Add labels inside the slices
+    svg
+      .selectAll('labels')
+      .data(data_ready)
+      .enter()
+      .append('text')
+      .text(d => d.data[0])
+      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+      .style("text-anchor", "middle")
+      .style("font-size", "12px");
+
+    // Add title
+    svg
+      .append("text")
+      .attr("text-anchor", "middle")
+      .attr("y", -height / 2 - margin.top / 2 + 20) // <-- [CHANGED] Adjust title position for pie
+      .attr("x", 0)
+      .attr("font-size", "16px")
+      .attr("font-weight", "bold")
+      .text("Repeat Customers");
     }
   }
 }
