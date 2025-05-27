01552e8df6ff5080c775e56583d3fec77232afc9
ravenusmc <mcuddy77@gmail.com>
2025-05-27 16:55:06 -0400

    added to explanation component

--- client/src/components/random/ExplainAgeTicketPrice.vue
+++ client/src/components/random/ExplainAgeTicketPrice.vue
@@ -3,7 +3,9 @@
     <div class="explanation-div">
       <p>
         Ok, the graph to the left is not what I thought it would be. I thought that I would get a graph, with some 
-        data points and a best fit line. 
+        data points and a best fit line. I guess in a way that's what I got. But the data points just didn't come out 
+        the way that I thought that they would. Now, at this point, I'll also say that AI suggested making this graph. However, 
+        I did start to learn about how I can use linear regression which is actually a new type of graph for me. 
       </p>
     </div>
   </div>
